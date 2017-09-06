b57054266037995967e6b8cc79d51fd9b62fcc5c
Kaushal Modi <kaushal.modi@gmail.com>
2017-09-06 16:37:51 -0400

    Remove FSF copyright until this package makes into GNU Elpa or Org

--- ox-blackfriday.el
+++ ox-blackfriday.el
@@ -1,28 +1,8 @@
 ;;; ox-blackfriday.el --- Blackfriday Flavored Markdown Back-End for Org Export Engine  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2017 Free Software Foundation, Inc.
-
 ;; Authors: Matt Price <moptop99@gmail.com>
 ;;          Kaushal Modi <kaushal.modi@gmail.com>
-;;          Lars Tveito <larstvei@ifi.uio.no>
 ;; URL: https://github.com/kaushalmodi/ox-hugo
-;; Package-Requires: ((emacs "24.5"))
-;; Keywords: org, markdown, blackfriday
-
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; GNU Emacs is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
--- ox-hugo.el
+++ ox-hugo.el
@@ -1,28 +1,11 @@
 ;;; ox-hugo.el --- Hugo Markdown Back-End for Org Export Engine  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2017 Free Software Foundation, Inc.
-
 ;; Authors: Kaushal Modi <kaushal.mod@gmail.com>
 ;;          Matt Price <moptop99@gmail.com>
 ;; URL: https://github.com/kaushalmodi/ox-hugo
 ;; Package-Requires: ((emacs "24.5"))
 ;; Keywords: Org, markdown, docs
 ;; Version: 0.1
-
-;; This file is part of GNU Emacs.
-
-;; GNU Emacs is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; GNU Emacs is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 

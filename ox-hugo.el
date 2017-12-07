34787be3b1705bfcc2a3ffd6e9594eba89d4f4ca
Kaushal Modi <kaushal.modi@gmail.com>
2017-12-07 11:34:01 -0500

    Correct the minimum requirement for ox-hugo
    It works fine on emacs 24.4 too.
    
    (Tested on emacs 24.3, but fails there as I use advice-add and other
    cool stuff from 24.4 onwards.. no point downgrading ox-hugo just so
    that it works with the old 24.3.)

--- ox-hugo.el
+++ ox-hugo.el
@@ -3,7 +3,7 @@
 ;; Authors: Kaushal Modi <kaushal.mod@gmail.com>
 ;;          Matt Price <moptop99@gmail.com>
 ;; URL: https://ox-hugo.scripter.co
-;; Package-Requires: ((emacs "24.5") (org "9.0"))
+;; Package-Requires: ((emacs "24.4") (org "9.0"))
 ;; Keywords: Org, markdown, docs
 ;; Version: 0.6
 

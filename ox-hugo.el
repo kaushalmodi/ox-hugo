358929bc6e8a916a61448d42592b7f034bd8e171
Kaushal Modi <kaushal.modi@gmail.com>
2018-08-10 19:14:35 -0400

    Revert "Move auto-export on save stuff to new ox-hugo-sugar.el"
    This reverts commit 4e931513455cc30bb0522f8dd44ec31a1398cd64.
    
    This move needs to be better planned out.. this commit broke
    auto-loading of ox-hugo using org-hugo-export-wim-to-md-after-save.

--- ox-hugo.el
+++ ox-hugo.el
@@ -77,9 +77,8 @@
 (require 'ob-core)                      ;For `org-babel-parse-header-arguments'
 (declare-function org-hugo-pandoc-cite--parse-citations-maybe "ox-hugo-pandoc-cite")
 
-(require 'ox-hugo-sugar)
-
 (defvar ffap-url-regexp)                ;Silence byte-compiler
+
 
 ;; Using the correct function for getting inherited Org tags.
 ;; Starting Org 9.2, `org-get-tags' returns all the inherited tags
@@ -136,6 +135,20 @@ front-matter that's used after Pandoc Citation parsing.")
 Pandoc understands meta-data only in YAML format.  So when Pandoc
 Citations are enabled, Pandoc is handed over the file with this
 YAML front-matter.")
+
+(defvar org-hugo-allow-export-after-save t
+  "Enable flag for `org-hugo-export-wim-to-md-after-save'.
+When nil, the above function will not export the Org file to
+Hugo-compatible Markdown.
+
+This variable is usually set to nil by the user in
+`org-capture-before-finalize-hook' and set to t again in
+`org-capture-after-finalize-hook', so that the export does not
+happen as soon as a new post is created using Org capture.
+
+Note that the export after save will not work until
+`org-hugo-export-wim-to-md-after-save' is added to the
+`after-save-hook' by the user.")
 
 (defvar org-hugo-blackfriday-options
   '("taskLists"
@@ -3648,6 +3661,19 @@ approach)."
                               (format "%s: %s" f-or-b-name msg)))))))
               (when do-export
                 (org-hugo-export-to-md async subtree visible-only)))))))))
+
+;;;###autoload
+(defun org-hugo-export-wim-to-md-after-save ()
+  "Fn for `after-save-hook' to run `org-hugo-export-wim-to-md'.
+
+The export is also skipped if `org-hugo-allow-export-after-save'
+is nil.  This variable is intended to be toggled dynamically in
+`org-capture-before-finalize-hook' and
+`org-capture-after-finalize-hook' hooks.  See the ‘Auto-export on
+Saving’ section in this package's documentation for an example."
+  (save-excursion
+    (when org-hugo-allow-export-after-save
+      (org-hugo-export-wim-to-md))))
 
 ;;;###autoload
 (defun org-hugo-debug-info ()

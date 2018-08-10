0e9c312cdd99589f2fe5cb3406d60ede6f73a03f
Kaushal Modi <kaushal.modi@gmail.com>
2018-08-10 17:21:08 -0400

    Update .dir-locals.el
    (Not sure why I had that extra (nil . ..) nesting..)

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,5 @@
-(nil
- . ((nil . ((indent-tabs-mode . nil)
-            (fill-column . 70)
-            (sentence-end-double-space . t)))
-    (org-mode . ((mode . auto-fill)))
-    (markdown-mode . ((mode . auto-fill)))))
+((nil . ((indent-tabs-mode . nil)
+         (fill-column . 70)
+         (sentence-end-double-space . t)))
+ (org-mode . ((mode . auto-fill)))
+ (markdown-mode . ((mode . auto-fill))))

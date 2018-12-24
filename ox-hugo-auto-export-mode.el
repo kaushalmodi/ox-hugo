9c8af09ee5c304c554c13318227de30dccf2accf
Evgeni Kolev <evgenysw@gmail.com>
2018-12-24 12:25:01 -0500

    Use `real-this-command` to determine if Org Capture is in progress

--- ox-hugo-auto-export-mode.el
+++ ox-hugo-auto-export-mode.el
@@ -20,37 +20,13 @@
 
 ;;; Code:
 
-(defvar org-hugo--org-capture-active-flag nil
-  "Non-nil means that an Org Capture is in progress.
-
-This variable is used to prevent auto-exports when saving Org
-Captures (applicable if creating new posts using Org Capture
-using the per-subtree export flow).")
-
 (defun org-hugo-export-wim-to-md-after-save ()
   "Function for `after-save-hook' to run `org-hugo-export-wim-to-md'.
 
-The exporting happens only if `org-hugo--org-capture-active-flag'
-is nil (i.e. Org Capture is not is progress)."
-  (unless org-hugo--org-capture-active-flag
+The exporting happens only when Org Capture is not in progress."
+  (unless (eq real-this-command 'org-capture-finalize)
     (save-excursion
       (org-hugo-export-wim-to-md))))
-
-;;; Org Capture Hook Functions
-;; Disallow auto-exporting when saving Org Captures.
-(defun org-hugo--disallow-auto-export-during-capture ()
-  "Function for `org-capture-before-finalize-hook'.
-
-Set `org-hugo--org-capture-active-flag' to t to prevent the
-`ox-hugo' auto-exports from happening when saving captures."
-  (setq org-hugo--org-capture-active-flag t))
-
-(defun org-hugo--allow-auto-export-after-capture ()
-  "Function for `org-capture-after-finalize-hook'.
-
-Set `org-hugo--org-capture-active-flag' back to nil so that the
-auto-export on save can resume working"
-  (setq org-hugo--org-capture-active-flag nil))
 
 ;;;###autoload
 (define-minor-mode org-hugo-auto-export-mode
@@ -60,15 +36,9 @@ auto-export on save can resume working"
   (if org-hugo-auto-export-mode
       ;; When the mode is enabled
       (progn
-        (add-hook 'after-save-hook #'org-hugo-export-wim-to-md-after-save :append :local)
-        (with-eval-after-load 'org-capture
-          (add-hook 'org-capture-before-finalize-hook #'org-hugo--disallow-auto-export-during-capture)
-          (add-hook 'org-capture-after-finalize-hook #'org-hugo--allow-auto-export-after-capture :append)))
+        (add-hook 'after-save-hook #'org-hugo-export-wim-to-md-after-save :append :local))
     ;; When the mode is disabled
-    (remove-hook 'after-save-hook #'org-hugo-export-wim-to-md-after-save :local)
-    (when (featurep 'org-capture)
-      (remove-hook 'org-capture-before-finalize-hook #'org-hugo--disallow-auto-export-during-capture)
-      (remove-hook 'org-capture-after-finalize-hook #'org-hugo--allow-auto-export-after-capture))))
+    (remove-hook 'after-save-hook #'org-hugo-export-wim-to-md-after-save :local)))
 
 
 (provide 'ox-hugo-auto-export-mode)

e3dac33bf0581493e7cfe5157a1d34183a12b4f0
Kaushal Modi <kaushal.modi@gmail.com>
2019-01-03 12:51:39 -0500

    Add Requirements section in Meta

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,5 @@
          (sentence-end-double-space . t)))
  (org-mode . ((eval . (auto-fill-mode 1))))
  (markdown-mode . ((eval . (auto-fill-mode 1))))
- ("doc"
-  . ((org-mode . ((eval . (org-hugo-auto-export-mode))))))
  ("test/site/content-org"
   . ((org-mode . ((eval . (org-hugo-auto-export-mode)))))))

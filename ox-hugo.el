1322e71caa51a90f9bef794d94d7d47f9ff823d2
Kaushal Modi <kaushal.modi@gmail.com>
2021-01-18 11:08:52 -0500

    Stop interpreting DEADLINE property as Hugo expiryDate
    This now-removed feature disallowed using the Org DEADLINE property to
    serve as a deadline.. say "deadline for finishing an
    article". Ironically, deadlines set with such intentions caused the
    article to get expired after that set date.
    
    Additionally, the expiryDate can still be set using the
    sub-tree posts.
    
    For that reason, now the DEADLINE special property is no longer
    interpreted as the Hugo expiryDate
    
    Fixes https://github.com/kaushalmodi/ox-hugo/issues/415 .
    
    breaking-change

--- ox-hugo.el
+++ ox-hugo.el
@@ -1335,10 +1335,6 @@ cannot be formatted in Hugo-compatible format."
                           (org-entry-get (point) "SCHEDULED"))
                      ;; Get the date from the "SCHEDULED" property.
                      (org-entry-get (point) "SCHEDULED"))
-                    ((and (equal date-key :hugo-expirydate)
-                          (org-entry-get (point) "DEADLINE"))
-                     ;; Get the date from the "DEADLINE" property.
-                     (org-entry-get (point) "DEADLINE"))
                     (t ;:hugo-lastmod, :hugo-publishdate, :hugo-expirydate
                      (org-string-nw-p (plist-get info date-key)))))
          (date-nocolon (cond

cb5d29ef9ef3f10aa1dbb3bb11f0882b612b4e2c
Kaushal Modi <kaushal.modi@gmail.com>
2022-01-04 13:38:25 -0500

    Use .dir-locals.el for the org-roam/org-id tests
    Move the location of the test Org files so that it has it's own .dir-locals.el

---
+++ test/site/content-org/org-roam/.dir-locals.el
@@ -0,0 +1,4 @@
+((org-mode . ((org-hugo-base-dir . "../../")
+              (org-hugo-section . "org-roam")
+              (org-export-with-author . nil)
+              (eval . (org-hugo-auto-export-mode)))))

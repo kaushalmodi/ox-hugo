aa20b868f81cf11a1fc78d51fbd3d8b86999f5b2
Kaushal Modi <kaushal.modi@gmail.com>
2022-05-12 01:25:42 -0400

    chore: Bump tomelr version
    v0.4.3 adds support for TOML table arrays in nested TOML tables, and
    fixes the indentation when exporting multi-line TOML strings.

--- ox-hugo.el
+++ ox-hugo.el
@@ -3,7 +3,7 @@
 ;; Author: Kaushal Modi <kaushal.modi@gmail.com>
 ;;         Matt Price <moptop99@gmail.com>
 ;; Version: 0.11.1
-;; Package-Requires: ((emacs "26.3") (tomelr "0.3.0"))
+;; Package-Requires: ((emacs "26.3") (tomelr "0.4.3"))
 ;; Keywords: Org, markdown, docs
 ;; URL: https://ox-hugo.scripter.co
 

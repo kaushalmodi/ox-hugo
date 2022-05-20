78fc25178bbf9aad0a8f9ad7024494e8810a58b9
Kaushal Modi <kaushal.modi@gmail.com>
2022-05-20 09:18:26 -0400

    minor(doc): Doc-string tweaks

--- ox-hugo.el
+++ ox-hugo.el
@@ -2444,7 +2444,7 @@ holding export options."
 
 ;;;; Inline Src Block
 (defun org-hugo-inline-src-block (inline-src-block _contents _info)
-  "Transcode INLINE-SRC-BLOCK object into Hugo-compatible Markdown format.
+  "Transcode INLINE-SRC-BLOCK object into HTML.
 
 Escape Hugo shortcodes if present in this element's value."
   (let* ((lang (org-element-property :language inline-src-block))
@@ -2452,7 +2452,8 @@ Escape Hugo shortcodes if present in this element's value."
                 (org-element-property :value inline-src-block)
                 lang)))
     (org-element-put-property inline-src-block :value code)
-    (format "<code class=\"inline-src language-%s\" data-lang=\"%s\">%s</code>" lang lang code)))
+    (format "<code class=\"inline-src language-%s\" data-lang=\"%s\">%s</code>"
+            lang lang code)))
 
 ;;;; Keyword
 (defun org-hugo-keyword (keyword contents info)

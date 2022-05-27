be13b91350f2bdff0d9793019139f3bc9494500c
Kaushal Modi <kaushal.modi@gmail.com>
2022-05-27 17:46:04 -0400

    wip: Start the test export upon running debug.sh

--- test/debug.sh
+++ test/debug.sh
@@ -3,4 +3,7 @@
 TEST_ENABLED=1
 export TEST_ENABLED
 
-emacs -Q -l "setup-ox-hugo.el"
+emacs -Q \
+      -l "setup-ox-hugo.el" \
+      site/content-org/temp.org \
+      --eval "(org-hugo-export-wim-to-md :all-subtrees)" &
